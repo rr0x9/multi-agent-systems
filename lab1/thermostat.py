e383f9e456118bedd5a2b7c234c8da2b838b88db
rr0x9 <220593292+rr0x9@users.noreply.github.com>
2026-09-15 20:23:50 +0100

    thermostat agent

--- lab1/thermostat.py
+++ lab1/thermostat.py
@@ -2,21 +2,31 @@ import asyncio
 import random
 
 temp = 20
+heater = False
 
 class Agent():
     async def run(self):
         while True:
-            if temp == 20:
-                print("Temp is good")
+            global heater
+            if temp < 20:
+                heater = True
+                print(f"temp({temp}) heater is ON")
+            elif temp > 20:
+                heater = False
+                print(f"temp({temp}) heater is OFF")
             else:
-                print("Temp is bad")
+                print("Temp is perfect")
             await asyncio.sleep(1)
 
 async def environment():
     while True:
         global temp
-        temp = random.randrange(12, 28)
-        print(temp)
+        if heater:
+            temp += random.randint(1, 2)
+            print(f"temp({temp}) has increased")
+        else:
+            temp -= random.randint(1, 2)
+            print(f"temp({temp}) has desreased")
         await asyncio.sleep(1)
 
 async def main():
